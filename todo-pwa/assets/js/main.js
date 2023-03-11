800af292d254efc2918878663b116a4d17881ae8
dan-carroll <dcarroll809@gmail.com>
2023-03-10 19:22:21 -0500

    Added code to install app and hide install button when/if installed.

--- todo-pwa/assets/js/main.js
+++ todo-pwa/assets/js/main.js
@@ -68,3 +68,36 @@ const deleteTodo = async (event, id) => {
 
 // Display todos on app start (load)
 window.onload = getTodos;
+
+
+
+
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+buttonInstall.addEventListener('click', async () => {
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+  // Optionally, send analytics event with outcome of user choice
+  console.log(`User response to the install prompt: ${outcome}`);
+  // We've used the prompt, and can't use it again, throw it away
+  deferredPrompt = null;
+});
+
+window.addEventListener('appinstalled', () => {
+  // Hide the install button
+  document.getElementById('buttonInstall').style.visibility= 'hidden';
+  // Clear the deferredPrompt so it can be garbage collected
+  deferredPrompt = null;
+  // Optionally, send analytics event to indicate successful install
+  console.log('PWA was installed');
+});
